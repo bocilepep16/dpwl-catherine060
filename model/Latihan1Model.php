75ef894baeb26cd233b9a0ec6cbbdcfb9a7f4669
Catherine Audreylia Diony <2522500060@mahasiwa.atmaluhur.ac.id>
2026-09-21 17:36:10 +0700

    menambhakan field dan nilai pada file Latihan1Model.php

---
+++ model/Latihan1Model.php
@@ -0,0 +1,13 @@
+<?php
+
+class Latihan1Model
+{
+    public function getAllMhs()
+    {
+        return [
+            ['nama' => 'Prita', 'nim' => '3022501'],
+            ['nama' => 'Cath', 'nim' => '3022502'],
+            ['nama' => 'Anjel', 'nim' => '3022503']
+        ];
+    }
+}
