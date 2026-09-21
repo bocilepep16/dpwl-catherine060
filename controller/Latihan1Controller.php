39398c9447d3ec687631e3b3478a740e395d0f5c
Catherine Audreylia Diony <2522500060@mahasiwa.atmaluhur.ac.id>
2026-09-21 17:55:26 +0700

    rename file Latihan1Controller

---
+++ controller/Latihan1Controller.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once 'model/Latihan1Model.php';
+
+class Latihan1Controller
+{
+    public function index()
+    {
+        $model = new Latihan1Model();
+        $datamhs = $model->getAllMhs();
+
+        require './view/latihan1view.php';
+    }
+}
