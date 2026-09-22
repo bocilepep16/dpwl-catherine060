92d3c1ec177a32aac158eab10e928e6dae26120d
Catherine Audreylia Diony <2522500060@mahasiwa.atmaluhur.ac.id>
2026-09-22 20:46:32 +0700

    menambahkan nilai alamat dan telp

--- model/Latihan1Model.php
+++ model/Latihan1Model.php
@@ -5,9 +5,12 @@ class Latihan1Model
     public function getAllMhs()
     {
         return [
-            ['nama' => 'Prita', 'nim' => '3022501'],
-            ['nama' => 'Cath', 'nim' => '3022502'],
-            ['nama' => 'Anjel', 'nim' => '3022503']
+            ['nama' => 'Prita', 'nim' => '3022501', 'alamat' => 'Jl. Semabung Bangka',
+            'telp'   => '0821134'],
+            ['nama' => 'Cath', 'nim' => '3022502', 'alamat' => 'Jl. Glodok Sumatera',
+            'telp'   => '0831187'],
+            ['nama' => 'Anjel', 'nim' => '3022503', 'alamat' => 'Jl. Batu Mentok',
+            'telp'   => '0812232']
         ];
     }
 }
