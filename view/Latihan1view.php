5596634d752638826f41369f7042aaf27975c0c5
Catherine Audreylia Diony <2522500060@mahasiwa.atmaluhur.ac.id>
2026-09-22 20:50:26 +0700

    menambahkan kode agar muncul tampilan tabel

--- view/Latihan1view.php
+++ view/Latihan1view.php
@@ -4,8 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Daftar Mahasiswa</title>
+    <style>
+        table{
+            border-collapse: collapse;
+            width: 900px;
+        }
+        table, th, td{
+            border:1px solid black;
+        }
+        th, td{
+            padding:8px;
+            text-align:left;
+        }
+        th{
+            background:#f2f2f2;
+        }
+    </style>
 </head>
 <body>
+
 
     <h2>Daftar Mahasiswa</h2>
 
